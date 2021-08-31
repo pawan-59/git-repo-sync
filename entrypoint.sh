abb17992f401d8c61afb198c3e2db5d43fedf5f6
pawan-59 <pawan@devtron.ai>
2021-08-31 12:25:03 +0530

    fourth commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
-sed -i "s/LTAG=.*/LTAG=\"main";/" manifests/installation-script"
+sed -i "s/LTAG=.*/LTAG=\"main";/" manifests/installation-script
 
-sed -i "s/url:.*/url: /"https://gitee.com/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml"
+sed -i "s/url:.*/url: https://gitee.com/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
 
-sed -i "s/url:.*/url: /"https:\/\/gitee.com\/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml"
+sed -i "s/url:.*/url: https:\/\/gitee.com\/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
 git add .
 git commit -m "updated github link to gitee"
 case "${GITHUB_EVENT_NAME}" in
