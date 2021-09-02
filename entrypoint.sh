91edb8c4529b3e76c92ea1d5599a5613994e0b2e
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2021-09-02 18:24:12 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,5 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
-echo "manifests"
-ls manifests
-echo "/manifests/install"
-ls manifests/install
 
-sed -i "s/LTAG=.*/LTAG=\"main\";/" manifests/installation-script
-
-sed -i "s/REPO_RAW_URL=.*/REPO_RAW_URL= https:\/\/gitee.com\/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/installation-script
-
-sed -i "s/url:.*/url: https:\/\/gitee.com\/pawan_06d2\/devtr\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
-
-sed -i "29 s/value:.*/value: https:\/\/gitee.com\/pawan_06d2\/devtr.git/" manifests/yamls/migrator.yaml
-
-sed -i "65 s/value:.*/value: https:\/\/gitee.com\/pawan_06d2\/devtr.git/" manifests/yamls/migrator.yaml
-
-sed -i "103 s/value:.*/value: https:\/\/gitee.com\/pawan_06d2\/devtr.git/" manifests/yamls/migrator.yaml
-
-sed -i "141 s/value:.*/value: https:\/\/gitee.com\/pawan_06d2\/devtr.git/" manifests/yamls/migrator.yaml
-
-git config --global user.email "pawan@devtron.ai"
-git config --global user.name "pawan_06d2"
-
-git commit -am "updated github link to gitee 1"
-echo "manifests"
-ls manifests
-echo "/manifests/install"
-ls manifests/install
 case "${GITHUB_EVENT_NAME}" in
     push)
         git push -f --all target
@@ -38,7 +12,3 @@ case "${GITHUB_EVENT_NAME}" in
         break
         ;;
 esac
-echo "manifests"
-ls manifests
-echo "/manifests/install"
-ls manifests/install
