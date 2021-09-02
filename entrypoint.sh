4578533fe5c6efa88287ee34df5b2a34518dd6d4
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2021-09-02 18:30:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
+git config --global user.email "pawan@devtron.ai"
+git config --global user.name "pawan_06d2"
+
 case "${GITHUB_EVENT_NAME}" in
     push)
         git push -f --all target
