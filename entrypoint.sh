56283f25946da6e8f7a9d2303730a4d244816f4a
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2021-09-15 22:26:45 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+#git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
 
 git clone https://github.com/pawan-59/devtron.git
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@gitee.com/pawan_06d2/devtr.git
