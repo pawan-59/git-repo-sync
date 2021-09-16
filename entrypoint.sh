81467441faa2f2994dbf86e52a2713b418193cd8
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2021-09-16 09:31:08 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
-#git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
-
 git clone https://github.com/pawan-59/devtron.git
 cd devtron
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@gitee.com/pawan_06d2/devtr.git
+git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+
 git checkout main
 git remote -v
 
@@ -20,26 +19,12 @@ sed -i "103 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/git-sensor.git/
 
 sed -i "141 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/lens.git/" manifests/yamls/migrator.yaml
 
-git config --global user.email "pawan@devtron.ai"
-git config --global user.name "pawan_06d2"
+git config --global user.email ${INPUT_USER_EMAIL}
+
+git config --global user.name ${INPUT_USER_NAME}
+
 
 git add .
 git commit -m "updated github link to gitee"
 
 git push -f --all target
-
-#case "${GITHUB_EVENT_NAME}" in
-#    release)
-#        git push -f target main
-#        ;;
-#    push)
-#        git push -f --all target
-#        git push -f --tags target
-#        ;;
- #   delete)
- #       git push -d target ${GITHUB_EVENT_REF}
- #       ;;
- #   *)
- #       break
- #       ;;
-#esac
