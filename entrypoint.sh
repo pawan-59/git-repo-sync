7317af497f21995a70de2f2746a4bc18961fe528
Pawan Kumar <karanraj11052000@gmail.com>
2022-05-09 16:30:26 +0530

    devtron chart sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,13 @@
-git clone https://github.com/pawan-59/devtron.git
-cd devtron
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+git clone INPUT_SOURCE_REPO
+git clone INPUT_TARGET_REPO
+cd helm-chart
+git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
 
 git checkout main
 git remote -v
 
-sed -i "s/LTAG=.*/LTAG=\"main\";/" manifests/installation-script
-
-sed -i "s/REPO_RAW_URL=.*/REPO_RAW_URL= https:\/\/gitee.com\/devtron-labs\/devtron\/raw/" manifests/installation-script
-
-sed -i "s/url:.*/url: https:\/\/gitee.com\/devtron-labs\/devtron\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
-
-sed -i "29 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
-
-sed -i "65 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
-
-sed -i "103 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/git-sensor.git/" manifests/yamls/migrator.yaml
-
-sed -i "141 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/lens.git/" manifests/yamls/migrator.yaml
-
-git config --global user.email ${INPUT_TARGET_USEREMAIL}
-
-git config --global user.name ${INPUT_TARGET_USERNAME}
-
-
+cp -r $INPUT_SOURCE_DIR $INPUT_TARGET_DIR
 git add .
-git commit -m "updated github link to gitee"
+git commit -m "Synching Repo"
 
-git push -f --all target
+git push target main
