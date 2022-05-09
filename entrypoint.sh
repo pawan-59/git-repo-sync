4d51026591a2a7a6423ed43f53541982a4c7ec1c
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-05-09 17:20:19 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-git clone INPUT_SOURCE_REPO
-git clone INPUT_TARGET_REPO
+git clone $INPUT_SOURCE_REPO
+git clone $INPUT_TARGET_REPO
 cd helm-chart
 git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
 
@@ -9,7 +9,7 @@ git remote -v
 git config --global user.email ${INPUT_TARGET_USEREMAIL}
 git config --global user.name ${INPUT_TARGET_USERNAME}
 
-cp -r ../${{INPUT_SOURCE_DIR}} $INPUT_TARGET_DIR
+cp -r ../${INPUT_SOURCE_DIR} $INPUT_TARGET_DIR
 git add .
 git commit -m "Synching Repo"
 
