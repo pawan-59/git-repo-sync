bad82784c8f10f93ea1d592d0ad6fd9a0d5c7dae
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-05-10 18:46:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,30 @@
-git clone $INPUT_SOURCE_REPO
-git clone $INPUT_TARGET_REPO
-cd $INPUT_WORKING_DIR
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
+git clone https://github.com/devtron-labs/devtron.git
+cd devtron
+git remote add target https://${INPUT_GITEE_TARGET_USERNAME}:${INPUT_GITEE_TARGET_TOKEN}@${INPUT_GITEE_TARGET_URL#https://}
 
 git checkout main
 git remote -v
 
-git config --global user.email ${INPUT_TARGET_USEREMAIL}
-git config --global user.name ${INPUT_TARGET_USERNAME}
+sed -i "s/LTAG=.*/LTAG=\"main\";/" manifests/installation-script
 
-cp -r ../${INPUT_SOURCE_DIR} $INPUT_TARGET_DIR
+sed -i "s/REPO_RAW_URL=.*/REPO_RAW_URL=\"https:\/\/gitee.com\/devtron-labs\/devtron\/raw\/\";/" manifests/installation-script
+
+sed -i "s/url:.*/url: https:\/\/gitee.com\/devtron-labs\/devtron\/raw\/main\/manifests\/installation-script/" manifests/install/devtron-installer.yaml
+
+sed -i "29 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
+
+sed -i "65 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/devtron.git/" manifests/yamls/migrator.yaml
+
+sed -i "103 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/git-sensor.git/" manifests/yamls/migrator.yaml
+
+sed -i "141 s/value:.*/value: https:\/\/gitee.com\/devtron-labs\/lens.git/" manifests/yamls/migrator.yaml
+
+git config --global user.email ${INPUT_GITEE_TARGET_USEREMAIL}
+
+git config --global user.name ${INPUT_GITEE_TARGET_USERNAME}
+
+
 git add .
-git commit -m "Synching Repo"
+git commit -m "updated github link to gitee"
 
-git push target main
+git push -f --all target
