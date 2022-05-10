c2a2fd26f8f4c2d39c8ea60384bb3a139d3c1d97
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-05-10 14:12:11 +0530

    Update chart-sync.sh

--- chart-sync.sh
+++ chart-sync.sh
@@ -1,13 +1,13 @@
 git clone $INPUT_SOURCE_REPO
 git clone $INPUT_TARGET_REPO
 cd $INPUT_WORKING_DIR
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_REPO#https://}
+git remote add target https://${INPUT_GIT_TARGET_USERNAME}:${INPUT_GIT_TARGET_TOKEN}@${INPUT_GIT_TARGET_REPO#https://}
 
 git checkout main
 git remote -v
 
-git config --global user.email ${INPUT_TARGET_USEREMAIL}
-git config --global user.name ${INPUT_TARGET_USERNAME}
+git config --global user.email ${INPUT_GIT_TARGET_USEREMAIL}
+git config --global user.name ${INPUT_GIT_TARGET_USERNAME}
 
 cp -r ../${INPUT_SOURCE_DIR} $INPUT_TARGET_DIR
 git add .
